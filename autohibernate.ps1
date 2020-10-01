29b279772a1f5575440f2c0e139480c5fb09fe38
reinistihovs <33620345+reinistihovs@users.noreply.github.com>
2020-10-01 17:58:08 +0300

    Create autohibernate.ps1

---
+++ autohibernate.ps1
@@ -0,0 +1,18 @@
+##This prevents laptops from overheating in the bag, when disconnectinfg from dcok, while the lid is closed.
+## Add this script in task scheduler,
+##Command:
+##C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+##Arguments:
+##-NoProfile -Executionpolicy bypass -file "C:\Windows\System32\autohibernate.ps1"
+##Trigger every 1 minute
+
+#Check if laptop is unplugged
+If (((Get-CimInstance win32_battery).BatteryStatus) -ne 2 ) {
+  #check if there are any active screens, unfortunatelly its almost impossible to get status of the Lid.
+  If (((Get-CimInstance -Namespace root\wmi -ClassName wmiMonitorID).Active) ) {
+    exit
+  } else {
+  #Put laptop into hibernation
+  shutdown /h
+  }
+}
